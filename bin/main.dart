d9761f67ccea7c80d3318e7be4fc04dda7a6964e
sirily11 <sirily1997@gmail.com>
2020-08-31 23:55:12 +0800

    fixed bugs

--- bin/main.dart
+++ bin/main.dart
@@ -6,7 +6,7 @@ void main(List<String> arguments) async {
   tz.initializeTimeZones();
   var shanghai = tz.getLocation('Asia/Shanghai');
   var now = tz.TZDateTime.now(shanghai);
-  if (now.hour > 0 && now.hour < 24) {
+  if (now.hour > 7 && now.hour < 22) {
     var shdl = ShangHaiDisneyLand();
     await shdl.writeToCSV();
   } else {
